d447dd3d24abdb5265872d9255bffac07a091560
Silmi <129129210+SilmiCamilia@users.noreply.github.com>
2025-10-01 11:15:58 +0700

    coba

---
+++ jobsheet6/ifCetakKRSNoPresensi.java
@@ -0,0 +1,19 @@
+package jobsheet6;
+
+import java.util.Scanner;
+
+public class ifCetakKRSNoPresensi {
+    public static void main(String[] args) {
+        Scanner imup = new Scanner(System.in);
+        System.out.println("--- Cetak Krs SIAKAD ---");
+        System.out.println("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = imup.nextBoolean();
+        
+        if(uktLunas){
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+            
+        }
+    }
+}
+
