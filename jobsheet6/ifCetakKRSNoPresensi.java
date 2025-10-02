d480e5112a5fce8ad35df0338c173adea5ba967f
Silmi <129129210+SilmiCamilia@users.noreply.github.com>
2025-10-02 18:05:53 +0700

    tugas1

--- jobsheet6/ifCetakKRSNoPresensi.java
+++ jobsheet6/ifCetakKRSNoPresensi.java
@@ -8,19 +8,10 @@ public class ifCetakKRSNoPresensi {
         System.out.println("--- Cetak Krs SIAKAD ---");
         System.out.println("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = imup.nextBoolean();
-
-        // Versi IF-ELSE
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
-        }
-
-        // 🔹 Versi Ternary Operator
         String pesan = uktLunas 
                         ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA"
                         : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu";
+
         System.out.println("\n=== Versi Ternary ===");
         System.out.println(pesan);
 
